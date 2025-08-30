c20c1787e266a3da21106d6c414f9921616666da
KOTO28 <k.tonton128@gmail.com>
2025-08-30 09:36:17 +0900

    chromeの存在確認を追加

--- chrome-guest.bat
+++ chrome-guest.bat
@@ -1,4 +1,12 @@
 @echo off
 set CHROME_PATH="%PROGRAMFILES%\Google\Chrome\Application\chrome.exe"
 
+:: Check if Chrome exists
+if not exist %CHROME_PATH% (
+    echo Error: Chrome not found.
+    echo Path: %CHROME_PATH%
+    pause
+    exit /b 1
+)
+
 %CHROME_PATH% --guest %~1
